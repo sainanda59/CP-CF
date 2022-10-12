110098f376f9b710dd35fa7faf8a7d4a968fefba
Animesh-21082002 <81861940+Animesh-21082002@users.noreply.github.com>
2022-10-12 09:47:56 +0530

    Create Number_of_Good_Pairs.cpp

---
+++ 01_Leetcode_FAANG/Animesh_Nayak/Number_of_Good_Pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j])
+                    c++;
+            }
+        }
+        return c;
+    }
+};
