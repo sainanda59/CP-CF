dd9dc888b934f7c9e4448f846006b590e8775f15
Npatro1729 <87572421+Npatro1729@users.noreply.github.com>
2022-10-13 15:18:37 +0530

    added solution to Two Sum

---
+++ 01_Leetcode_FAANG/Naren_Patro/Two_Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        unordered_map<int , int> m;
+      for(int i =0 ; i< nums.size();i++){
+          if(m.find(target - nums[i]) != m.end()){
+              v.push_back(m[target - nums[i]]);
+              v.push_back(i);
+              return v;
+          }
+          m[nums[i]] = i;
+      }
+        return v;
+    }
+};
