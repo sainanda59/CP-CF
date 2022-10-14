017e5b0cf011a4737928ec89eefa8e77db6935d4
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-14 22:07:00 +0530

    Update Divide_Array.cpp

--- 01_Leetcode_FAANG/Bikash Dalai/Divide_Array.cpp
+++ 01_Leetcode_FAANG/Bikash Dalai/Divide_Array.cpp
@@ -1,17 +1,21 @@
-class Solution {
-public:
-    bool divideArray(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]++;
-        }
-        for(auto x:m)
-        {
-            if(x.second%2!=0)
-                return false;
-        }
-        
-        return true;
-    }
-};
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, number,A[100],i;
+    cout << "Enter the number of element of an array(max 100):";
+    cin>>n;
+    cout<<"Enter the elements of an array:";
+    for(i=0;i<n;i++)
+        cin>>A[i];
+    cout<<"Enter the number you want to divide all the elements of an array:";
+    cin>>number;
+    for(i=0;i<n;i++)
+        A[i]=A[i]/number;
+    cout<<"Enter final array after dividing all elements with "<<number<<" :\n";
+    for(i=0;i<n;i++)
+        cout<<A[i]<<" ";
+    return 0;
+}
