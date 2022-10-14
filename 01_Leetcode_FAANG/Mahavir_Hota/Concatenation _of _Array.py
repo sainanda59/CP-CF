0913a094cee84ba22aa27a6f1b8c6dc654623b52
Soham Chowdhury <chowdhurysoham007@gmail.com>
2022-10-14 21:57:02 +0530

    Update Concatenation _of _Array.py

--- 01_Leetcode_FAANG/Mahavir_Hota/Concatenation _of _Array.py
+++ 01_Leetcode_FAANG/Mahavir_Hota/Concatenation _of _Array.py
@@ -1,9 +1,23 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        for num in nums:
-            ans.append(num)
+# Example 1: Use concatenate() to join two arrays
+con = np.concatenate((arr, arr1))
+print(con)
 
-        for num in nums:
-            ans.append(num)
-        return ans 
+# Example 2: Use concatenate() with axis
+con = np.concatenate((arr, arr1), axis=1)
+print(con)
+
+# Example 3: Use np.stack() function to Join Arrays
+con = np.stack((arr, arr1), axis=1)
+print(con)
+
+# Example 4: Use np.hstack() function
+con = np.hstack((arr, arr1))
+print(con)
+
+# Example 5: Use np.vstack() function 
+con = np.vstack((arr, arr1))
+print(con)
+
+# Example 6: Use np.dstack() function to Stacking Along Height (depth)
+con = np.dstack((arr, arr1))
+print(con)
