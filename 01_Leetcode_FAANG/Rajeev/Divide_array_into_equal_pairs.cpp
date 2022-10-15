f29e280fd788e835c255836b26650de86ab5e147
204Rajeev <96741998+204Rajeev@users.noreply.github.com>
2022-10-15 22:51:09 +0530

    Create Divide_array_into_equal_pairs.cpp

---
+++ 01_Leetcode_FAANG/Rajeev/Divide_array_into_equal_pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<nums.size();i+=2)
+            if(nums[i]!=nums[i-1])return false;
+        
+        return true;
+    }
+};
