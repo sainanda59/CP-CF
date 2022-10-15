6ddee2a35a316c2481f8a6e977ced162f2492f33
Mahavir Prasad Hota <37869969+Mahavirhota@users.noreply.github.com>
2022-10-15 21:28:05 +0530

    Create Two_Sum.py

---
+++ 01_Leetcode_FAANG/Mahavir_Hota/Two_Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        values = {}
+        for idx, value in enumerate(nums):
+            if target - value in values:
+                return [values[target - value], idx]
+            else:
+                values[value] = idx
