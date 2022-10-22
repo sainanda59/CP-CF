79d7d3fb586361ba27ac1366679cb11892014a83
BikashDalai <116306583+BikashDalai@users.noreply.github.com>
2022-10-22 21:30:01 +0530

    Add files via upload

---
+++ 01_Leetcode_FAANG/SpydyCoder/single_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            x^=nums[i];
+        }
+        return x;
+        
+        return 0;
+    }
+};
